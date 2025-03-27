7c321bcb1615443df9e3c6694daa05334989da98
Quang Duong Nguyen <qnguyen2@macalester.edu>
2025-03-27 00:33:44 -0700

    type

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,20 @@ fn main() {
         stdout.flush().unwrap();
 
         stdin.read_line(&mut input).unwrap();
-        match input.trim() {
-            "exit 0" => break,
-            input if input.starts_with("echo ") => {
-                println!("{}", &input[5..])
+        match input.trim().split_once(' ') {
+            Some((command, args)) => match command {
+                "exit" => match args {
+                    "0" => break,
+                    _ => println!("{}: command not found", input.trim()),
+                },
+                "echo" => println!("{}", args),
+                "type" => match args {
+                    "echo" | "exit" | "type" => println!("{} is a shell builtin", args),
+                    _ => println!("{}: not found", args),
+                },
+                &_ => println!("{}: command not found", input.trim()),
             },
-            _ => {
-                println!("{}: command not found", input.trim());
-            }
-        }
-        if input.trim() == "exit 0" {
-            break;
+            None => println!("{}: command not found", input.trim()),
         }
     }
 }
